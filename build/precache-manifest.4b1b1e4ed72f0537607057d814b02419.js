d64c78993b30e297dd0cb9f2bce47b0ee4f7eebb
Akash2001k <kaushikakash116@gmail.com>
2023-01-20 14:46:24 +0530

    Done

---
+++ build/precache-manifest.4b1b1e4ed72f0537607057d814b02419.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1908406e2ae22195feb7",
+    "url": "/static/css/main.37d370ee.chunk.css"
+  },
+  {
+    "revision": "1908406e2ae22195feb7",
+    "url": "/static/js/main.1908406e.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "27c21812ed81363b425d542820b93a55",
+    "url": "/index.html"
+  }
+];
